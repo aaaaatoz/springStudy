3eaabbf7eb006cee02514ae04fd147cfd936fdcb
aaaaatoz <rafa.xu.au@gmail.com>
2015-12-03 22:31:05 +1100

    constructor injection

--- src/com/rafa/spring/TriangleDraw.java
+++ src/com/rafa/spring/TriangleDraw.java
@@ -3,17 +3,26 @@ package com.rafa.spring;
 
 public class TriangleDraw {
 
+
+    public TriangleDraw(String type, int height) {
+        this.type = type;
+        this.height = height;
+    }
+
+    private String type;
+    private int height;
+
+    public int getHeight() {
+        return height;
+    }
+
     public String getType() {
         return type;
     }
 
-    public void setType(String type) {
-        this.type = type;
-    }
-
-    private String type;
 
     public void draw(){
         System.out.println("My Type is " + getType());
+        System.out.println("My height is " + getHeight());
     }
 }
